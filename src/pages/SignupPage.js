2428bbc51b56e0fcaff7790697eed30d804f5ab1
pmani-250 <78112386+pmani-250@users.noreply.github.com>
2025-03-10 19:52:18 -0700

    Add Sign up and Login Pages

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,20 @@
+import {useNavigate, Link} from "react-router-dom";
+import "../styles/SignupPage.css";
+
+const SignupPage = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="login-container">
+            <h1 className="login-title">Sign Up</h1>
+            <form>
+                <input type="email" className="login-email" placeholder="email"/>
+                <input type="password" className="login-password" placeholder="password"/>
+                <input type="password" className="password-confirm" placeholder="confirm password"/>
+            </form>
+            <button className="signup-button" onClick={() => navigate("/journal")}>Sign Up</button>
+            <p className="signin-text">Already have an account? <Link to="/LoginPage" className="Signin-Link">Sign in!</Link></p>
+        </div>
+    );
+};
+
+export default SignupPage;
